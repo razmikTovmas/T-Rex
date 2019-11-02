3d7cc6081651307d9396b0f848a2331d99ed8c23
razmikTovmas <razmik.tovmas@gmail.com>
2019-11-02 22:23:49 +0400

    Added new prefabs to T-Rex runner.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,28 +9,60 @@ public class GameManager : MonoBehaviour
     public float restartDelay = 1.0f;
     public ulong score;
     public Text scoreText;
+    public Text highestScoreText;
+    public Text currentScoreText;
+    public Text newText;
+
+    public Canvas gameOver;
+    static ulong highestScore = 0;
 
     long counter;
-    bool isGameOver = false;
+    float timescale;
 
+    void Start()
+    {
+        timescale = Time.timeScale;
+        gameOver.enabled = false;
+        //newText.enabled = false;
+        Debug.Log(highestScore);
+    }
 
     public void GameOver()
     {
-        if (isGameOver) return;
-        isGameOver = true;
+        Time.timeScale = 0.0f;
         Debug.Log("Game Over!");
-        Invoke("Restart", restartDelay);
+
+        currentScoreText.text = scoreText.text;
+        ulong c = ulong.Parse(currentScoreText.text);
+        Debug.Log(highestScore);
+        Debug.Log(c);
+        Debug.Log("HS: " + (c > highestScore).ToString());
+        if (c > highestScore)
+        {
+            highestScoreText.text = currentScoreText.text;
+            highestScore = c;
+            newText.enabled = true;
+        }
+        else
+        {
+            highestScoreText.text = highestScore.ToString();
+            newText.enabled = false;
+        }
+
+        gameOver.enabled = true;
+
     }
 
     void Restart()
     {
         // SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("Main");
+        gameOver.enabled = false;
+        Time.timeScale = timescale;
     }
 
     void FixedUpdate()
     {
-        if (isGameOver) return;
         if (counter > 30)
         {
             ++score;
@@ -38,5 +70,9 @@ public class GameManager : MonoBehaviour
             counter = 0;
         }
         ++counter;
+        if (score % 50 == 0)
+        {
+            Time.timeScale += timescale * 0.01f;
+        }
     }
 }
